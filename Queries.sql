c5ba0625361d6c5a5709952b6443310ece5186c7
almerylondono <sheccidpaisa@hotmail.com>
2023-03-27 08:59:11 -0500

    Se modifica la documentación y se agrega otra información de query

--- Queries.sql
+++ Queries.sql
@@ -7,3 +7,13 @@ select pro.producto_id, pro.desc_producto, producto_vendido.max_valor_total
 from database_prueba_tecnica.dim_producto as pro
 inner join producto_vendido on pro.producto_id = producto_vendido.producto_id
 ;
+
+
+select pro.desc_producto, cli.numero, cli.ciudad, cli.departamento, cli.edad,
+cli.nombre || ' ' || cli.primer_apellido || ' ' || cli.segundo_apellido as nombre_completo,
+sum(fact.cantidad) cantidad, sum(fact.valor) valor, sum(fact.valor_total) valor_total
+from database_prueba_tecnica.fact_venta fact
+inner join database_prueba_tecnica.dim_cliente cli on fact.cliente_id = cli.cliente_id
+inner join database_prueba_tecnica.dim_producto pro on fact.producto_id = pro.producto_id
+group by pro.desc_producto, cli.numero, cli.ciudad, cli.departamento, cli.edad,
+cli.nombre, cli.primer_apellido, cli.segundo_apellido
